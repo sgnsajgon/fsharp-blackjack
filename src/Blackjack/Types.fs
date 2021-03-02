f0418e39f4e41f7907a7ecfbf44a9147fc445089
Wojciech Pokrzepa <wojciech.pokrzepa@netent.com>
2021-03-02 18:08:03 +0100

    Add basic types which model game workflow

---
+++ src/Blackjack/Types.fs
@@ -0,0 +1,45 @@
+﻿namespace Blackjack
+
+type HandId = HandId of uint
+
+type BetAmount = BetAmount of uint
+
+type SideBet =
+    | PerfectPair of BetAmount
+    | Twenty1plus3 of BetAmount
+
+type Bet =
+    | Main of BetAmount
+    | SideBet of BetAmount
+
+type HandBet = { HandId: HandId; Bets: Bet list  }
+
+type DealAction = HandBet list
+
+type Action =
+    | Deal of DealAction
+    | Hit
+    | Stand
+    | Double
+    | Surrender
+    | Insurance
+    | InsuranceAll
+    | EvenMoney
+    | EvenMoneyAll
+    
+type GameEvent =
+    | TableHandDealt
+    | Blackjack
+    | InsuranceOffered
+    | InsuranceAllOffered
+    | EvenMoneyOffered
+    | EvenMoneyAllOffered
+    | HandHit
+    | HandStood
+    | HandBusted
+    | HandWon
+    | HandPushed
+    | HandSurrendered
+    | HandLost
+    
+type ProcessAction = Action -> GameEvent list
